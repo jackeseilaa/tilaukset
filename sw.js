968b2f45ddf0eddf5fa286188634c46259677f56
jackeseilaa <jacke.seilaa@gmail.com>
2026-05-26 11:49:43 +0000

    REVERT: Palautetaan täsmälleen V9.148

--- sw.js
+++ sw.js
@@ -1,12 +1,10 @@
-const CACHE_VERSION = 'v153';
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
-self.addEventListener('fetch', event => {});
+
+self.addEventListener("fetch", event => {
+});
